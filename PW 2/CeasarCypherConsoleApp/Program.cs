64bdc6f991212da1fadae736f9d0ab452eac5f40
Sap <127602959+saps1z@users.noreply.github.com>
2023-03-26 22:55:50 +0300

    Second Practise / Prakticheskaya #2

---
+++ PW 2/CeasarCypherConsoleApp/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace CaesarCipherConsole
+{
+    // Создаём класс Шифр Цезаря, к которому мы и будем обращаться
+    class CaesarCipher
+    {
+        // Метод зашифровки строки
+        public string Encryption( string input, int key )
+        {
+            string result = string.Empty;       // Создаём локальную переменную строку и присваеваем ей пустое значение
+                                                // чтобы компилятор не выдавал ошибки
+            foreach ( char symbol in input )    // Запускаем цикл перебирающие все символы в строке
+            {
+                if ( char.IsLetter( symbol ) )
+                {
+                    char shiftedSymbol = ( char ) ( ( ( symbol - 'A' ) + key ) % 26 + 'A' );    // Формула, по которой происходит сдвиг букв
+                    result += shiftedSymbol;
+                }
+                else
+                {
+                    result += symbol;
+                }
+            }
+
+            return result;          // Получаем нашу обработанную строку
+        }
+
+        // Метод расшифровки строки, делаем буквально тоже самое, что и при шифровке, но меняем формулу
+        public string Decryption( string input, int key )
+        {
+            string result = string.Empty;
+
+            foreach ( char symbol in input )
+            {
+                if ( char.IsLetter( symbol ) )
+                {
+                    char shiftedSymbol = ( char ) ( ( ( symbol - 'A' ) - key + 26 ) % 26 + 'A' );
+                    result += shiftedSymbol;
+                }
+                else
+                {
+                    result += symbol;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    // Класс самой программы
+    class Program
+    {
+        static void Main()
+        {
+            CaesarCipher cipher = new CaesarCipher();   // Создаём новый объект CaesarCipher
+            string inputString;                         // Переменная, в которой будет хранится наша строка
+            int key;                                    // Переменная, где мы указываем кол-во символов для сдвига
+
+            // С запуском программы, выдаём информацию
+            Console.WriteLine( "Добро пожаловать в программу, способную" );
+            Console.WriteLine( "шифровать и расшифровывать строки шифром Цезаря !" );
+            Console.WriteLine();
+
+            // Цикл, где наш код будет повторяться, чтобы Пользователю было удобнее шифровать строки
+            while (true)
+            {
+                // Цикл, где мы проверяем нашу строку
+                while (true)
+                {
+                    Console.Write("Введите текст: ");       // Просим ввести тескт
+                    inputString = Console.ReadLine();       // Считываем строку и записываем в переменную inputString
+
+                    // Если строка пуста
+                    if (inputString == string.Empty)
+                    {
+                        // Выводим текст о том, что такого допускать нельзя
+                        Console.WriteLine("Строка не может быть пустой! Пожалуйста, введите текст");
+                    }
+                    // Если строка имеет текст
+                    else
+                    {
+                        break;      // Значит всё хорошо и мы можем покинуть цикл
+                    }
+                }
+
+                // Цикл, лимитирующий ошибочные действия Пользователя
+                while (true)
+                {
+                    Console.Write("Введите число ( от 1 до 25 ), которое послужит ключём шифрования: ");        // Просим Пользователя ввести число от 1 до 25
+                    key = int.Parse(Console.ReadLine());                                                        // Считываем это число и записываем в переменную key
+
+                    // Если ключ привысил лимит, то выдаём ошибку
+                    if (key > 25)
+                    {
+                        Console.WriteLine("Английский алфавит содержит 26 букв!");
+                        Console.WriteLine("Шифр Цезаря в этом случае может сдвинуть буквы только на 25 позиций...");
+                    }
+                    // Если ключ равен 0, делаем тоже самое
+                    else if (key == 0)
+                    {
+                        Console.WriteLine("Нулевой буквы не существует");
+                    }
+                    // Если ключ отрицательный, тоже выдаём ошибку
+                    else if (key < 0)
+                    {
+                        Console.WriteLine("Отрицательные цифры писать нельзя");
+                    }
+                    // Если всё нормально, делаем выход из цикла
+                    else { break; }
+                }
+
+                // Показываем зашифрованную строку
+                string encryptedString = cipher.Encryption(inputString, key);
+                Console.WriteLine("Зашифрованная строка: " + encryptedString);
+
+                // Показываем расшифрованную строку
+                string decryptedString = cipher.Decryption(encryptedString, key);
+                Console.WriteLine("Расшифрованная строка: " + decryptedString);
+
+                // Делаем отступ в одну строку
+                Console.WriteLine();
+                // И повторяем цикл
+            }
+        }
+    }
+}
---
+++ PW 2/DoubleKorenConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace DoubleKorenConsoleApplication
+{
+    // Класс квадратного уравнения
+    class DoubleKoren
+    {
+        private double a, b, c, x1, x2;      // Создаём переменные квадратного уравнения
+
+        private double Discriminante(  )        // Создаём приватный метод для вычисления дискрименанта
+        {
+            return b * b - 4 * a * c;           // Формула дискрименанта b^2 - 4ac
+        }
+
+        public void SetA( double a ) { this.a = a; }        // Глобальная функция для установки переменной a
+        public void SetB( double b ) { this.b = b; }        // Глобальная функция для установки переменной b
+        public void SetC( double c ) { this.c = c; }        // Глобальная функция для установки переменной c
+
+        // Глобальная функция, вычисляющая корни уравнения x1 и x2
+        public void CalculateRoots( )   
+        {
+            if ( (double)Discriminante() < 0 )                 // Если дискрименант меньше нуля, тогда выдаём ошибку о том, что уравнение не имеет корней
+            {
+                Console.WriteLine("D < 0  =>  Уравнение не имеет корней");
+            }
+            else if ( (double)Discriminante() == 0 )           // Но если дискрименант всё же равен нулю, даём решение только одного корня
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine("D = 0  =>  Уравнение имеет один корень: X = " + x1);
+            }
+            else                                               // В других же случаях D > 0 мы проводим полное решение уравнения
+            {
+                x1 = ( -b + Math.Sqrt( (double)Discriminante() ) ) / ( 2 * a );
+                x2 = ( -b - Math.Sqrt( (double)Discriminante() ) ) / ( 2 * a );
+
+                Console.WriteLine("Корни уравнения: X1 = {0}, X2 = {1}", x1, x2);
+            }
+        }
+
+        // Глобальные функции для получения переменных уравнения
+        public double GetA( ) { return a; }             // Получаем коэффициент a
+        public double GetB( ) { return b; }             // Получаем коэффициент b
+        public double GetC( ) { return c; }             // Получаем коэффициент c
+        public double GetX1( ) { return x1; }           // Получаем первый корень уравнения x1
+        public double GetX2( ) { return x2; }           // Получаем второй корень уравнения  x2
+
+    }
+
+    // Класс самой программы
+    class Program
+    {
+        static void Main()
+        {
+            // Выводим сообщение о том, что программа стартовала и отсупаем одну строку
+            Console.WriteLine("РЕШАТЕЛЬ КВАДРАТНЫХ УРАВНЕНИЙ");
+            Console.WriteLine();
+
+            // Создаём объект решателя квадратного уравнения
+            DoubleKoren Reshatel = new DoubleKoren();
+
+            // Просим пользователя установить значение коэффициента a
+            Console.Write("Введите значение коэффициента a: ");
+            Reshatel.SetA( double.Parse(Console.ReadLine()) );
+
+            // Просим пользователя установить значение коэффициента b
+            Console.Write("Введите значение коэффициента b: ");
+            Reshatel.SetB( double.Parse(Console.ReadLine()) );
+
+            // Просим пользователя установить значение коэффициента c
+            Console.Write("Введите значение коэффициента c: ");
+            Reshatel.SetC( double.Parse(Console.ReadLine()) );
+
+            // После установки значений отступаем одну строку и выдаём ответ
+            Console.WriteLine();
+            // Предварительно показываем значение переменных коэффициентов
+            Console.WriteLine("Коэффициенты уравнения: A = " + Reshatel.GetA() + "; B = " + Reshatel.GetB() + "; C = " + Reshatel.GetC() );
+            // Используя публичные методы получения значений, используя формулу, вычисляем значение дискрименанта
+            Console.WriteLine("Дискрименант равен: D = " + ( Reshatel.GetB() * Reshatel.GetB() - 4 * Reshatel.GetA() * Reshatel.GetC() ) );
+            // И решаем само уравнение
+            Reshatel.CalculateRoots();
+
+            // В конце просим пользователя нажать клавишу для завершения программы
+            Console.ReadKey(true);
+        }
+    }
+}
